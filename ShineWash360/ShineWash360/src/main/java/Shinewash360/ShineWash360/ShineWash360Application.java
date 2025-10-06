3a7d0494f14155a5b3ba56180b77034df06dd161
shinewash360 <shinewash360@gmail.com>
2025-10-06 19:17:26 +0530

    add files

---
+++ ShineWash360/ShineWash360/src/main/java/Shinewash360/ShineWash360/ShineWash360Application.java
@@ -0,0 +1,13 @@
+package Shinewash360.ShineWash360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShineWash360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShineWash360Application.class, args);
+	}
+
+}
---
+++ ShineWash360/ShineWash360/src/test/java/Shinewash360/ShineWash360/ShineWash360ApplicationTests.java
@@ -0,0 +1,13 @@
+package Shinewash360.ShineWash360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShineWash360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
